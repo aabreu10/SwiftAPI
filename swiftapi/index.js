21cc13304cb288fa79341463b308525fdc14cfdd
Juan Abreu <abreujuan1710study@gmail.com>
2025-04-28 17:40:38 +0200

    initialization and compression

---
+++ swiftapi/index.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    minWidth: 800,
+    minHeight: 600,
+    backgroundColor: '#1e1e1e', 
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, 
+    },
+    title: "SwiftAPI", 
+  });
+
+  win.loadFile('index.html');
+
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
